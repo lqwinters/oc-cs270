0e0874fbcdfc8b691aeba9140490c9cfb5c1110b
Lindsey Winters <lwinters@olivetcollege.edu>
2013-02-12 15:02:04 -0500

    added route to app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+# Assignment 1: A haphazard page showing the most common HTML tags
+@app.route('/assignment-1')
+def assignment_1():
+    return redirect(url_for('static', filename='assignment-1.html'))
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
