989198023825bd9ff1fbb7abf2b8c997de61b7ee
Lindsey Winters <lwinters@olivetcollege.edu>
2013-03-01 12:56:27 -0500

    exercise 4.1 finished in assignment-4

---
+++ static/assignment-4.js
@@ -0,0 +1,31 @@
+// Assignment-4.js 
+// Ch.4 exercises 4.1-4.5
+
+// Exercise 4.1
+// Output: A table of the numbers 5 to 15 and thier squares and cubes, using alert
+
+alert('The following page contains a table of numbers 5-15 and both their sqaures/cubes');
+
+var sqaures = 
+
+document.write('<table border="1">');{
+	// For headers 
+	document.write('<tr><th>Numbers</th><th>Squared</th><th>Cubed</th></tr>'); 
+	// For rows
+	document.write('<tr><td>5</td><td>25</td><td>125</td></tr>');
+	document.write('<tr><td>6</td><td>36</td><td>216</td></tr>');
+	document.write('<tr><td>7</td><td>49</td><td>343</td></tr>');
+	document.write('<tr><td>8</td><td>64</td><td>512</td></tr>');
+	document.write('<tr><td>9</td><td>81</td><td>729</td></tr>');
+	document.write('<tr><td>10</td><td>100</td><td>1000</td></tr>');
+	document.write('<tr><td>11</td><td>121</td><td>1331</td></tr>');
+	document.write('<tr><td>12</td><td>144</td><td>1728</td></tr>');
+	document.write('<tr><td>13</td><td>169</td><td>2197</td></tr>');
+	document.write('<tr><td>14</td><td>196</td><td>2744</td></tr>');
+	document.write('<tr><td>15</td><td>225</td><td>3375</td></tr>')
+	document.write('</table>');
+}
+
+
+
+
