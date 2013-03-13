d0fde7044b633be5552154324d5dc599c5ac6521
Lindsey Winters <lwinters@olivetcollege.edu>
2013-03-13 19:57:25 -0400

    Assignment 4 complete!

--- static/assignment-4.js
+++ static/assignment-4.js
@@ -6,25 +6,116 @@
 
 alert('The following page contains a table of numbers 5-15 and both their sqaures/cubes');
 
-var sqaures = 
+document.write('<h1>Exercise 4.1</h1>');
+document.write('<table border="1">'); {
+	// For headers in table
+	document.write('<tr><th>Number</th><th>Squared</th><th>Cubed</th></tr>'); 
 
-document.write('<table border="1">');{
-	// For headers 
-	document.write('<tr><th>Numbers</th><th>Squared</th><th>Cubed</th></tr>'); 
-	// For rows
-	document.write('<tr><td>5</td><td>25</td><td>125</td></tr>');
-	document.write('<tr><td>6</td><td>36</td><td>216</td></tr>');
-	document.write('<tr><td>7</td><td>49</td><td>343</td></tr>');
-	document.write('<tr><td>8</td><td>64</td><td>512</td></tr>');
-	document.write('<tr><td>9</td><td>81</td><td>729</td></tr>');
-	document.write('<tr><td>10</td><td>100</td><td>1000</td></tr>');
-	document.write('<tr><td>11</td><td>121</td><td>1331</td></tr>');
-	document.write('<tr><td>12</td><td>144</td><td>1728</td></tr>');
-	document.write('<tr><td>13</td><td>169</td><td>2197</td></tr>');
-	document.write('<tr><td>14</td><td>196</td><td>2744</td></tr>');
-	document.write('<tr><td>15</td><td>225</td><td>3375</td></tr>')
-	document.write('</table>');
+	// For loop to get numbers 5-15 and both their sqaures/cubes
+	var square, cube;
+	for (i = 5; i<16; i++) {
+		square = i * i;
+		cube = square * i;
+		// For rows in table
+		document.write('<tr><td>' + i + '</td><td>' + square + '</td><td>' + cube + '</td></tr>');
+	}
+document.write('</table><br />');
+
 }
+
+// Exercise 4.2
+// Output: The first 20 Fibonacci numbers. You must use document.write to produce the output.
+
+document.write('<h1>Exercise 4.2</h1>');
+document.write('<p>First 20 Fibonacci Numbers<p>');
+document.write('<table border="1">'); {
+	// For headers in table
+	document.write('<tr><th>Number</th><th>Fibonacci Number</th></tr>'); 
+		document.write('<tr><td>0</td><td>0</td></tr>');
+		document.write('<tr><td>1</td><td>1</td></tr>');
+	// For loop to get numbers 5-15 and both their sqaures/cubes
+	var F0 = 0, F1 = 1, Fn;
+	for (i = 2; i<=20; i++) {
+		Fn = F0 + F1;
+			// For rows in table
+			document.write('<tr><td>' + i + '</td><td>' + Fn+ '</td></tr>');
+			F0 = F1;
+			F1 = Fn;
+	}
+document.write('</table><br />');
+
+}
+
+// Exercise 4.3
+// Input: Three numbers, using prompt to get each one
+// Output: The largest of the three numbers.
+document.write('<h1>Exercise 4.3</h1>');
+
+var number1 = prompt('Enter a number', ''); 
+var number2 = prompt('Enter a second number', ''); 
+var number3 = prompt('Enter a third number', ''); 
+value1 = parseFloat(number1); 
+value2 = parseFloat(number2); 
+value3 = parseFloat(number3); 
+MaxVal = Math.max(value1, value2, value3); 
+
+document.write('The maximum number you have entered is: ' + MaxVal + '<br />');
+
+// Exercise 4.4
+// Modify exercise 4.2 to use prompt to input a number n that is the number of
+// the Fibonacci number required as output.
+document.write('<h1>Exercise 4.4</h1>');
+
+document.write('<table border="1">'); {
+	var f1 = 0, f2 = 1, fn;
+	number = prompt('How many fibonacci sequence numbers would you like to see? (2-20)', '');
+
+	if (number >= 2 && number <= 20) {
+	   	document.write('First ' + number + ' fibonacci sequence numbers <br /><br />');
+		// For headers in table
+		document.write('<tr><th>Number</th><th>Fibonacci Number</th></tr>'); 
+			document.write('<tr><td>0</td><td>0</td></tr>');
+			document.write('<tr><td>1</td><td>1</td></tr>');
+
+		for (i = 2; i <= number; i++)  {
+        	fn = f1 + f2;
+        	document.write('<tr><td>' + i + '</td><td>' + fn + '</td></tr>');
+        	f1 = f2;
+        	f2 = fn; 
+      	}
+   	}
+	else
+   		document.write("Error - number not in the range 2-20");
+
+document.write('</table><br />');
+}
+
+// Exercise 4.5
+// Input: A text string, using promt.
+// Output: Either "Valid Name" or "Invalid Name", depending on whether the input 
+// names fit the required format which is,
+// Last name, first name, middle intial
+// where neither of the names can have more than 15 characters. 
+document.write('<h1>Exercise 4.5</h1>');
+
+var msg = prompt('Please enter your Last Name, First Name and Middle Initial', '');
+
+
+if (msg.match(/^[A-Z][a-z]{1,14}, [A-Z][a-z]{1,14}, [A-Z]\.?$/)) {
+    document.write('<p>Valid Entry!<p>');
+    alert('Valid');
+}
+else {
+	document.write('<p>Invalid Entry<p>');
+    alert('InValid');
+	
+}
+
+
+
+
+
+
 
 
 
